544c6b35c3f88a859780fbbd7c9f65dc9cd22b39
Dain-K <dksudi76@naver.com>
2022-12-11 17:39:34 +0900

    회원가입 형식 제한 로직 작성

--- js/join.js
+++ js/join.js
@@ -1,3 +1,87 @@
+$(document).ready(function () {
+  var idx = true;
+  $("#userError").hide();
+  $("#emailError2").hide();
+  $("#phoneError").hide();
+  $("#submit").click(function () {
+    const userClass = document.getElementById("username").classList;
+    const emailClass = document.getElementById("email").classList;
+    const phoneClass = document.getElementById("phone").classList;
+    $("#username").change(function () {
+      userClass.remove("active");
+      $("#userError").hide();
+    });
+    if ($("#username").val().length == 0) {
+      $("#username").focus();
+      $("#userError").show();
+      userClass.add("active");
+      idx = false;
+    }
+
+    if ($("#email").val().length == 0) {
+      $("#email").focus();
+      $("#emailError2").show();
+      emailClass.add("active");
+      idx = false;
+    }
+
+    if ($("#birth").val().length == 0) {
+      alert("생일을 입력하세요.");
+      $("#birth").focus();
+      idx = false;
+    }
+
+    $("#phone").change(function () {
+      phoneClass.remove("active");
+      $("#phoneError").hide();
+    });
+    if ($("#phone").val().length == 0) {
+      phoneClass.add("active");
+      $("#phoneError").show();
+      $("#phone").focus();
+      idx = false;
+    }
+
+    if (!$("select[name=gender]").val()) {
+      alert("성별을 선택하세요.");
+      $("select[name=gender]").focus();
+      idx = false;
+    }
+    if (!$("select[name=address]").val()) {
+      alert("거주지역을 선택하세요.");
+      $("select[name=address]").focus();
+      idx = false;
+    }
+    if (!$("select[name=education]").val()) {
+      alert("최종학력을 선택하세요.");
+      $("select[name=education]").focus();
+      idx = false;
+    }
+    if ($("select[name=education]").val() == "univ") {
+      if (!$("select[name=status]").val()) {
+        alert("상태를 선택하세요.");
+        $("select[name=status]").focus();
+        idx = false;
+      }
+      if ($("#university").val().length == 0) {
+        alert("대학교를 입력하세요.");
+        $("#university").focus();
+        idx = false;
+      }
+      if ($("#department").val().length == 0) {
+        alert("학과를 입력하세요.");
+        $("#department").focus();
+        idx = false;
+      }
+    }
+    if (!$("select[name=mbti]").val()) {
+      alert("mbti을 선택하세요.");
+      $("select[name=mbti]").focus();
+      idx = false;
+    }
+    return idx;
+  });
+});
 function oninputPhone(target) {
   target.value = target.value
     .replace(/[^0-9]/g, "")
@@ -7,12 +91,15 @@ function oninputPhone(target) {
 $(function () {
   /* 이메일 형식 맞는지 판단 */
   $("#emailError").hide();
+  var reg_email =
+    /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
   $("#email").change(function () {
     const str = $("#email").val();
     const className = document.getElementById("email").classList;
-    if (str.includes("@") && str.includes(".com")) {
+    if (reg_email.test(str)) {
       className.remove("active");
       $("#emailError").hide();
+      $("#emailError2").hide();
     } else {
       className.add("active");
       $("#emailError").show();
@@ -40,7 +127,7 @@ $(function () {
   });
 
   $("#selboxDirect").hide();
-  $("#school").change(function () {
+  $("#education").change(function () {
     // 대학교를 선택할 경우 나타남
     if ($(this).find("option:selected").val() == "univ") {
       $("#selboxDirect").show();
